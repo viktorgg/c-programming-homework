3ee82063a546dfc249c1bf043be0c58fbc1e50dc
viktorgg <viktor.g.g@abv.bg>
2016-10-15 13:14:42 +0300

    Create task01.c

---
+++ A/02/05/task01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,m=0,p=0;
+        scanf("%d",&n);
+        m=n*n;
+        p=n*n*n;
+        printf("\n%d",m);
+        printf("\n%d",p);
+}
